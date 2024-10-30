47d4accdd455c12d870df20a4aa5e5e3ac6bfb7e
ArkZani <60407722+ArkZani@users.noreply.github.com>
2024-10-30 11:51:06 -0700

    File to send email

---
+++ backend/flaskmail.py
@@ -0,0 +1,29 @@
+from flask import Flask, request, jsonify
+from flask_mail import Mail, Message
+
+app = Flask(__name__)
+app.config['MAIL_SERVER'] = 'your_mail_server'
+app.config['MAIL_PORT'] = 587
+app.config['MAIL_USE_TLS'] = True
+app.config['MAIL_USE_SSL'] = False
+app.config['MAIL_USERNAME'] = 'your_username'
+app.config['MAIL_PASSWORD'] = 'your_password'
+app.config['MAIL_DEFAULT_SENDER'] = 'rentersgrid@gmail.com'
+
+mail = Mail(app)
+
+@app.route('/send-email', methods=['POST'])
+def send_email():
+  data = request.get_json()
+  recipient_email = data.get('email')
+
+  if recipient_email:
+    msg = Message(
+        'do-not-reply',
+        sender='rentersgrid@gmail.com',
+        recipients=[recipient_email],
+        body='Here is your link: http://localhost:3000/resetpasswordupdate'
+    )
+    mail.send(msg)
+    return jsonify({'message': 'Email Sent!'}), 200
+  return jsonify({'message': 'Email not provided!'}), 400
