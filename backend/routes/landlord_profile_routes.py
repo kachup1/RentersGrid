b3fdeb1e22ce40db1feb8d9bc1c68b007ceebfa7
kachup1 <emailkarlahere@gmail.com>
2024-11-11 17:41:09 -0800

    commiting before pulling

--- backend/routes/landlord_profile_routes.py
+++ backend/routes/landlord_profile_routes.py
@@ -1,6 +1,6 @@
 # landlord_profile_routes.py
 from flask import Blueprint, request, jsonify
-from models.database import landlords_collection, ratings_collection
+from models.database import landlords_collection, ratings_collection,properties_collection
 from bson import ObjectId  # Import ObjectId for type checking
 
 landlord_profile_blueprint = Blueprint('landlord_profile', __name__)
@@ -21,18 +21,35 @@ def get_landlord_profile(landlord_id):
     # Convert ObjectId in landlord document
     landlord = serialize_mongo_document(landlord)
 
+    #getting properties associated with the landlord
+    properties = list(properties_collection.find({"landlordId": int(landlord_id)}))
+    properties = [serialize_mongo_document(prop)for prop in properties]
+
     # Fetch all reviews for this landlord and convert ObjectId fields
+    reviews = list(ratings_collection.find({"landlordId":int(landlord_id)}))
     reviews = [serialize_mongo_document(review) for review in ratings_collection.find({"landlordId": int(landlord_id)})]
 
     avg_rating = sum(r['score'] for r in reviews) / len(reviews) if reviews else None
+
+    # Calculate rating distribution, of how many of each score are given
+    rating_distribution = {
+        "Excellent": sum(1 for r in reviews if r['score'] == 5),
+        "Good": sum(1 for r in reviews if r['score'] == 4),
+        "Average": sum(1 for r in reviews if r['score'] == 3),
+        "Decent": sum(1 for r in reviews if r['score'] == 2),
+        "Poor": sum(1 for r in reviews if r['score'] == 1),
+    }
 
     landlord_data = {
         "landlordId": landlord.get("landlordId"),
         "name": landlord.get("name"),
         "type": landlord.get("type"),
-        "properties": landlord.get("propertyId", []),
+        "properties": properties,
         "averageRating": avg_rating,
         "reviewCount": len(reviews),
-        "reviews": reviews
+        "reviews": reviews,
+        "ratingDistribution":rating_distribution,
+        
+
     }
     return jsonify(landlord_data), 200
